b59d2f8e7047f36aeb200ee199681beeda949df8
Ian Robison <ian.m.robis@gmail.com>
2022-11-03 11:26:34 -0500

    Initial Commit

---
+++ FileParsing/Lexer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguage {
+    public class Lexer {
+
+        private List<Token> tokensForLine = new List<Token>();
+
+        internal List<Token> TokensForLine { get => this.tokensForLine; }
+
+
+        public Lexer () { }
+
+        public Lexer (string lineOfCode) {
+            
+        }
+
+        private void ParseLineOfCode (string lineOfCode) {
+            
+        }
+    }
+}
---
+++ FileParsing/Token.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguage {
+
+    internal class Token {
+        internal enum Type {
+            Identifier,
+            Number,
+            Operation,
+            DataType,
+        }
+
+        private readonly Type type;
+        private readonly string value;
+
+        internal Type TokenType { get => this.type; }
+
+        internal string Value { get => this.value; }
+
+        internal Token (Type type, string value) {
+            this.type = type;
+            this.value = value;
+        }
+
+        public override string ToString () {
+            return TokenType switch {
+                Type.Identifier => $"[id: \"{Value}\"]",
+                Type.Number => $"[num: {Value}]",
+                Type.Operation => $"[op: '{Value}']",
+                Type.DataType => $"[dt: \"{Value}\"]",
+                _ => "Invalid token"
+            };
+        }
+    }
+}
